c572e5cef4255913ea89da2536436301ef305613
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2025-12-28 17:12:45 +0900

    応答データをJsonファイルから取得する

--- HttpServerLibrary/Server.cs
+++ HttpServerLibrary/Server.cs
@@ -53,24 +53,47 @@ namespace HttpServerLibrary
         /// <summary> リクエスト受信時の処理 </summary>
         private void OnRequestReceived(IAsyncResult result)
         {
+            // リスナーの状態確認
             if (result.AsyncState is not HttpListener listener) return;
             if (!listener.IsListening) { return; }
 
             try
             {
+                // リクエストコンテキストの取得
                 HttpListenerContext context = listener.EndGetContext(result);
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
 
+                // レスポンスの設定
                 response.StatusCode = (int)HttpStatusCode.OK;
                 response.ContentType = "application/json";
                 response.ContentEncoding = Encoding.UTF8;
 
+                // レスポンスデータの取得
+                byte[] buffer = GetResponseData();
+
+                // レスポンスの書き込み
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
                 using var writer = new StreamWriter(response.OutputStream, Encoding.UTF8);
             }
             catch (Exception)
             {
                 throw;
+            }
+        }
+
+        /// <summary> レスポンスデータを取得する </summary>
+        private static byte[] GetResponseData()
+        {
+            if (File.Exists("response.json"))
+            {
+                string jsonResponse = File.ReadAllText("response.json");
+                return Encoding.UTF8.GetBytes(jsonResponse);
+            }
+            else
+            {
+                return Encoding.UTF8.GetBytes("{\"error\":\"response.json file not found.\"}");
             }
         }
     }
