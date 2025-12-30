a383e2d7c1ede394e8e3cd281676da375b5853e1
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2025-12-30 15:16:57 +0900

    サーバー開始機能をWPFに実装 ※コンソールプロジェクトは削除する

--- HttpServerWPF/MainWindow.xaml.cs
+++ HttpServerWPF/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace HttpServerWPF
 {
@@ -19,6 +10,10 @@ namespace HttpServerWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
--- HttpServerWPF/MainWindowViewModel.cs
+++ HttpServerWPF/MainWindowViewModel.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HttpServerService;
+using Reactive.Bindings;
+using Reactive.Bindings.Disposables;
+using Reactive.Bindings.Extensions;
 
 namespace HttpServerWPF
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase, IDisposable
     {
+        public ReactiveProperty<string> HostName { get; } = new ReactiveProperty<string>(string.Empty);
+        public ReactiveProperty<int> PortNo { get; } = new ReactiveProperty<int>(0);
+        public ReactiveProperty<string> Path { get; } = new ReactiveProperty<string>(string.Empty);
+        public ReactiveProperty<string> UserId { get; } = new ReactiveProperty<string>(string.Empty);
+        public ReactiveProperty<string> Password { get; } = new ReactiveProperty<string>(string.Empty);
+        public ReactiveProperty<string> LogText { get; } = new ReactiveProperty<string>(string.Empty);
+        public ReactiveProperty<string> StatusMessage { get; } = new ReactiveProperty<string>(string.Empty);
+
+        public ReactiveCommand LoadedCommand { get; } = new();
+        public ReactiveCommand SaveCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand StartCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand StopCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ClearMessageCommand { get; } = new ReactiveCommand();
+
+
+        public MainWindowViewModel()
+        {
+            SaveCommand.Subscribe(this.OnSaveButtonClicked).AddTo(_disposables);
+            StartCommand.Subscribe(this.OnStartButtonClicked).AddTo(_disposables);
+            StopCommand.Subscribe(this.OnStopButtonClicked).AddTo(_disposables);
+            LoadedCommand.Subscribe(this.OnLoaded).AddTo(_disposables);
+            //ClearMessageCommand.Subscribe(this.ClearMessage).AddTo(_disposables);
+
+            //// 通信履歴ファイルの監視を開始
+            //_logFileWatcher.FileChanged += OnLogFileChanged;
+        }
+
+        private void OnLoaded()
+        {
+            //throw new NotImplementedException();
+        }
+        private void OnSaveButtonClicked()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnStartButtonClicked()
+        {
+            Server.Instance.Start();
+        }
+
+        private void OnStopButtonClicked()
+        {
+            Server.Instance.Stop();
+        }
+
+
+        private readonly CompositeDisposable _disposables = new();
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
     }
 }
